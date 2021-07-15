054847170e663e338fd6ea28e4ec09c992696d8f
Felipe <felipevictor67@hotmail.com>
2021-07-15 03:23:27 -0300

    corrigindo deploy

--- thingspeak/settings/base.py
+++ thingspeak/settings/base.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import environ
 from pathlib import Path
 import django_heroku
 
@@ -21,18 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-env = environ.Env()
-
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-pw5z=9u^xt43xm9pk%3-2r!ex^-wt%9e6vac^#1k8(*xt_jf6v'
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-pw5z=9u^xt43xm9pk%3-2r!ex^-wt%9e6vac^#1k8(*xt_jf6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = env.bool("DEBUG", False)
+DEBUG = True
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -89,15 +83,11 @@ WSGI_APPLICATION = 'thingspeak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
-
-DATABASES = {
-    "default": env.db(),
 }
 
 
